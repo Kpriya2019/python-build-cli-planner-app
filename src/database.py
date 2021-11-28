2fedc9cb83ff16910a8212ca0021c121f747df00
priya <Kamatchipriya.Dhamodharan@domino-uk.com>
2021-11-28 19:44:59 +0000

    Polite remainder class added
    1ST phase

--- src/database.py
+++ src/database.py
@@ -1,4 +1,5 @@
 import csv
+from src.remainder import PoliteRemainder
 
 def list_reminders():
     f = open("reminders.csv", "r")
@@ -13,7 +14,8 @@ def list_reminders():
         print()
 
 def add_reminder(text):
+    remainder = PoliteRemainder().text
 
     with open('reminders.csv', 'a+', newline='\n') as file:
         writer = csv.writer(file)
-        writer.writerow([text])
+        writer.writerow([reminder.text])
--- src/reminder.py
+++ src/reminder.py
@@ -5,3 +5,11 @@ class PrefixedReminder:
     def __init__(self, prefix="Hey, don't forget to "):
         self.prefix = prefix
         self.text = prefix + '<placeholder_text>'
+
+class PoliteRemainder(PrefixedReminder):
+
+
+	def __init__(self,text):
+		super().__init__(self,prefix="please")
+		self.prefix =  prefix
+		
